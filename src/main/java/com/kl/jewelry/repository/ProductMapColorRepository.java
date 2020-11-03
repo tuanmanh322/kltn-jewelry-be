411f88b44168170372a8419e8637a14477ab5da6
tuandm <tuandm@itsol.vn>
2020-11-03 21:32:29 +0700

    done

--- src/main/java/com/kl/jewelry/repository/ProductMapColorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kl.jewelry.repository;
-
-import com.kl.jewelry.model.ProductMapColor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface ProductMapColorRepository extends JpaRepository<ProductMapColor, Long>, JpaSpecificationExecutor<ProductMapColor> {
-
-}
